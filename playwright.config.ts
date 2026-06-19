f5c88404e84a5afb56c08bc6213f09cb5f8b8950
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2026-06-19 10:51:28 -0500

    07_Trace_Viewer/032_WhatIsTraceViewer_
    - 32. What is Trace Viewer ?
    
    #07_Trace_Viewer/032_WhatIsTraceViewer_

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,15 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 5 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   ...(process.env.CI ? { workers: 1 } : {}),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
